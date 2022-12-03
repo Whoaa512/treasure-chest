f5c2bc82f7c4610212e29455da1ecd9f64e406b3
CJ Winslow <whoaa512@gmail.com>
2022-12-03 13:23:07 -0800

    Add the get route

--- warp-server.rs
+++ warp-server.rs
@@ -1,4 +1,4 @@
-use warp::{Filter, path, post};
+use warp::{Filter, path, post, get};
 use std::fs;
 
 // This function processes the file upload and writes the file to the specified path.
@@ -17,14 +17,37 @@ async fn put_file(filepath: String, file: impl warp::Buf) -> Result<impl warp::R
     Ok(warp::reply())
 }
 
+// This function reads the file at the specified path and returns its contents.
+async fn get_file(filepath: String) -> Result<impl warp::Reply, warp::Rejection> {
+    // Open the file in read-only mode.
+    let mut file = match fs::File::open(&filepath) {
+        Ok(file) => file,
+        Err(err) => return Ok(warp::reply::with_status(err.to_string(), warp::http::StatusCode::INTERNAL_SERVER_ERROR)),
+    };
+
+    // Read the file contents into a vector of bytes.
+    let mut contents = Vec::new();
+    if let Err(err) = file.read_to_end(&mut contents) {
+        return Ok(warp::reply::with_status(err.to_string(), warp::http::StatusCode::INTERNAL_SERVER_ERROR));
+    }
+
+    // Return the file contents as a response.
+    Ok(warp::reply::bytes(&contents))
+}
+
 #[tokio::main]
 async fn main() {
     // Set up the route that accepts file uploads.
-    let routes = post()
+    let put_routes = post()
         .and(path("put").and(path::param::<String>()))
         .and(warp::body::bytes())
         .and_then(put_file);
 
+    // Set up the route that returns the contents of a file.
+    let get_routes = get()
+        .and(path("get").and(path::param::<String>()))
+        .and_then(get_file);
+
     // Start the server.
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(put_routes.or(get_routes)).run(([127, 0, 0, 1], 3030)).await;
 }
